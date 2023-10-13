213c68239de04f86a3a32c4d0cff3819cb93e2b4
xenolit <xenolit.games@gmail.com>
2023-10-14 01:49:42 +0300

    Almost finished /time
    TODO: Add ArrayList with worlds for /time command

--- src/main/java/gq/bxteam/nexus/commands/list/TimeCommand.java
+++ src/main/java/gq/bxteam/nexus/commands/list/TimeCommand.java
@@ -3,6 +3,8 @@ package gq.bxteam.nexus.commands.list;
 import gq.bxteam.nexus.Nexus;
 import gq.bxteam.nexus.commands.CommandBase;
 import gq.bxteam.nexus.utils.TextUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -13,30 +15,54 @@ public class TimeCommand extends CommandBase implements CommandExecutor {
     }
     @Override
     protected void execute(CommandSender sender, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(TextUtils.applyColor(Nexus.getInstance().localeReader.getPrefix() + Nexus.getInstance().localeReader.getString("not-player")));
-            return;
+        if (sender instanceof Player) {
+
+            Player player = (Player) (sender);
+
+            if (!player.hasPermission("nexus.command.time")) {
+                player.sendMessage(TextUtils.applyColor(Nexus.getInstance().localeReader.getPrefix() + Nexus.getInstance().localeReader.getString("no-permission")));
+                return;
+            }
+
+            if (label.equalsIgnoreCase("day")) {
+                player.getLocation().getWorld().setTime(1000);
+                player.sendMessage(TextUtils.applyColor(Nexus.getInstance().localeReader.getPrefix() + Nexus.getInstance().localeReader.getString("time-success").replace("%t", "day")));
+            } else if (label.equalsIgnoreCase("night")) {
+                player.getLocation().getWorld().setTime(13000);
+                player.sendMessage(TextUtils.applyColor(Nexus.getInstance().localeReader.getPrefix() + Nexus.getInstance().localeReader.getString("time-success").replace("%t", "night")));
+            } else if (label.equalsIgnoreCase("midnight")) {
+                player.getLocation().getWorld().setTime(18000);
+                player.sendMessage(TextUtils.applyColor(Nexus.getInstance().localeReader.getPrefix() + Nexus.getInstance().localeReader.getString("time-success").replace("%t", "midnight")));
+            } else if (label.equalsIgnoreCase("noon")) {
+                player.getLocation().getWorld().setTime(6000);
+                player.sendMessage(TextUtils.applyColor(Nexus.getInstance().localeReader.getPrefix() + Nexus.getInstance().localeReader.getString("time-success").replace("%t", "noon")));
+            }
         }
 
-        Player player = (Player) (sender);
+        else
+        {
+            if (args[0] == null)
+            {
+                sender.sendMessage(TextUtils.applyColor(Nexus.getInstance().localeReader.getPrefix() + Nexus.getInstance().localeReader
+                        .getString("time-usage")));
+                return;
+            }
 
-        if (!player.hasPermission("nexus.command.time")) {
-            player.sendMessage(TextUtils.applyColor(Nexus.getInstance().localeReader.getPrefix() + Nexus.getInstance().localeReader.getString("no-permission")));
-            return;
-        }
+            World world = Bukkit.getWorld(args[0]);
 
-        if (label.equalsIgnoreCase("day")) {
-            player.getLocation().getWorld().setTime(1000);
-            player.sendMessage(TextUtils.applyColor(Nexus.getInstance().localeReader.getPrefix() + Nexus.getInstance().localeReader.getString("time-success").replace("%t", "day")));
-        } else if (label.equalsIgnoreCase("night")) {
-            player.getLocation().getWorld().setTime(13000);
-            player.sendMessage(TextUtils.applyColor(Nexus.getInstance().localeReader.getPrefix() + Nexus.getInstance().localeReader.getString("time-success").replace("%t", "night")));
-        } else if (label.equalsIgnoreCase("midnight")) {
-            player.getLocation().getWorld().setTime(18000);
-            player.sendMessage(TextUtils.applyColor(Nexus.getInstance().localeReader.getPrefix() + Nexus.getInstance().localeReader.getString("time-success").replace("%t", "midnight")));
-        } else if (label.equalsIgnoreCase("noon")) {
-            player.getLocation().getWorld().setTime(6000);
-            player.sendMessage(TextUtils.applyColor(Nexus.getInstance().localeReader.getPrefix() + Nexus.getInstance().localeReader.getString("time-success").replace("%t", "noon")));
+            if (label.equalsIgnoreCase("day")) {
+                world.setTime(1000);
+                sender.sendMessage(TextUtils.applyColor(Nexus.getInstance().localeReader.getPrefix() + Nexus.getInstance().localeReader.getString("time-success").replace("%t", "day")));
+            } else if (label.equalsIgnoreCase("night")) {
+                world.setTime(13000);
+                sender.sendMessage(TextUtils.applyColor(Nexus.getInstance().localeReader.getPrefix() + Nexus.getInstance().localeReader.getString("time-success").replace("%t", "night")));
+            } else if (label.equalsIgnoreCase("midnight")) {
+                world.setTime(18000);
+                sender.sendMessage(TextUtils.applyColor(Nexus.getInstance().localeReader.getPrefix() + Nexus.getInstance().localeReader.getString("time-success").replace("%t", "midnight")));
+            } else if (label.equalsIgnoreCase("noon")) {
+                world.setTime(6000);
+                sender.sendMessage(TextUtils.applyColor(Nexus.getInstance().localeReader.getPrefix() + Nexus.getInstance().localeReader.getString("time-success").replace("%t", "noon")));
+            }
         }
     }
 }
