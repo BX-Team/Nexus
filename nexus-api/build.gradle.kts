a9c90e75f8fb65851754816e49f55f07c65d283f
NONPLAY <76615486+NONPLAYT@users.noreply.github.com>
2024-11-17 01:51:00 +0300

    fix api repository

--- nexus-api/build.gradle.kts
+++ nexus-api/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 repositories {
     mavenCentral()
-    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
