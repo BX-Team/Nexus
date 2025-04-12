da121638aa1ae307b05963168c31dc4f3d1fd5a0
NONPLAY <76615486+NONPLAYT@users.noreply.github.com>
2025-04-13 02:09:39 +0300

    add compatibility service

--- nexus-core/src/main/java/org/bxteam/nexus/core/utils/ClassgraphUtil.java
+++ nexus-core/src/main/java/org/bxteam/nexus/core/utils/ClassgraphUtil.java
@@ -4,6 +4,7 @@ import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ClassInfo;
 import io.github.classgraph.ScanResult;
 import lombok.experimental.UtilityClass;
+import org.bxteam.nexus.core.annotations.compatibility.CompatibilityService;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.annotation.Annotation;
@@ -12,12 +13,21 @@ import java.util.function.Consumer;
 @UtilityClass
 public class ClassgraphUtil {
     public void scanClassesWithAnnotation(String packageName, @NotNull Class<? extends Annotation> annotation, Consumer<ClassInfo> consumer) {
+        CompatibilityService compatibilityUtil = new CompatibilityService();
+
         try (ScanResult scanResult = new ClassGraph()
                 .enableClassInfo()
                 .enableAnnotationInfo()
                 .acceptPackages(packageName)
                 .scan()) {
-            scanResult.getClassesWithAnnotation(annotation.getName()).forEach(consumer);
+            scanResult.getClassesWithAnnotation(annotation.getName()).forEach(classInfo -> {
+                try {
+                    Class<?> clazz = classInfo.loadClass();
+                    if (compatibilityUtil.isCompatible(clazz)) consumer.accept(classInfo);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
         }
     }
 }
